b09053ccd25d0b01b74a57bdc87fc995e84f6e21
y3sh <joshhibschman@gmail.com>
2018-06-29 14:19:16 -0500

    BX-19 node run de-duping

--- src/bxcommon/connections.py
+++ src/bxcommon/connections.py
@@ -217,6 +217,95 @@ class AbstractClient(object):
 
         self.tx_manager = TransactionManager(self)
 
+    def get_connection_class(self):
+        raise NotImplementedError()
+
+    # Handles incoming connections on the server socket
+    # Only allows MAX_CONN_BY_IP connections from each IP address to be initialized.
+    def handle_serversocket_connections(self):
+        log_verbose("new connection establishment starting")
+        try:
+            while True:
+                new_socket, address = self.serversocket.accept()
+                log_debug("new connection from {0}".format(address))
+                ip = address[0]
+
+                # If we have too many connections, then we close this new socket and move on.
+                if self.connection_pool.get_num_conn_by_ip(ip) >= MAX_CONN_BY_IP:
+                    log_err("The IP {0} has too many connections! Closing...".format(ip))
+                    new_socket.close()
+                else:
+                    log_debug("Establishing connection number {0} from {1}"
+                              .format(self.connection_pool.get_num_conn_by_ip(ip), ip))
+                    # The trusted bit here will be set when we get the application layer address.
+                    conn_cls = self.get_connection_class()
+                    self.init_client_socket(conn_cls, address[0], address[1], new_socket, setup=False)
+
+        except socket.error:
+            pass
+
+    def poll_loop(self):
+        try:
+            _, timeout = self.alarm_queue.time_to_next_alarm()
+            while True:
+                # Grab all events.
+                try:
+                    events = self.epoll.poll(timeout)
+                except IOError as ioe:
+                    if ioe.errno == errno.EINTR:
+                        log_verbose("got interrupted in epoll")
+                        continue
+                    raise ioe
+
+                for fileno, event in events:
+                    conn = self.connection_pool.get_byfileno(fileno)
+
+                    if conn is not None:
+                        # Mark this connection for close if we received a POLLHUP. No other functions will be called
+                        #   on this connection.
+                        if event & select.EPOLLHUP:
+                            conn.state |= ConnectionState.MARK_FOR_CLOSE
+
+                        if event & select.EPOLLOUT and not conn.state & ConnectionState.MARK_FOR_CLOSE:
+                            # If connect received EINPROGRESS, we will receive an EPOLLOUT if connect succeeded
+                            if not conn.state & ConnectionState.INITIALIZED:
+                                conn.state = conn.state | ConnectionState.INITIALIZED
+
+                            # Mark the connection as sendable and send as much as we can from the outputbuffer.
+                            conn.mark_sendable()
+                            conn.send()
+
+                    # handle incoming connection on the server port
+                    elif fileno == self.serversocketfd:
+                        self.handle_serversocket_connections()
+
+                    else:
+                        assert False, "Connection not handled!"
+
+                # Handle EPOLLIN events.
+                for fileno, event in events:
+                    # we already handled the new connections above, no need to handle them again
+                    if fileno != self.serversocketfd:
+                        conn = self.connection_pool.get_byfileno(fileno)
+
+                        if event & select.EPOLLIN and not conn.state & ConnectionState.MARK_FOR_CLOSE:
+                            # log_debug("Node.run", "recv event on {0}".format(conn.peer_desc))
+                            conn.recv()
+
+                        # Done processing. Close socket if it got put on the blacklist or was marked for close.
+                        if conn.state & ConnectionState.MARK_FOR_CLOSE:
+                            log_debug("Connection to {0} closing".format(conn.peer_desc))
+                            self.destroy_conn(fileno)
+                            if conn.is_persistent:
+                                self.alarm_queue.register_alarm(RETRY_INTERVAL, self.retry_init_client_socket, None,
+                                                                conn.__class__, conn.peer_ip, conn.peer_port, True)
+
+                timeout = self.alarm_queue.fire_ready_alarms(not events)
+
+            # Handle shutdown of this node.
+        finally:
+            self.cleanup_node()
+
     # Create and initialize a nonblocking server socket with at most 50 connections in its backlog,
     #   bound to an interface and port
     # Exit the program if there's an unrecoverable socket error (e.g. no more kernel memory)
@@ -351,6 +440,77 @@ class AbstractClient(object):
             log_debug("Retrying connection to {0}:{1}.".format(ip, port))
             self.init_client_socket(conn_cls, ip, port, sock, setup)
         return 0
+
+    def connect_to_peers(self):
+        raise NotImplementedError()
+
+    # Main loop of this Node. Returns when Node crashes or is stopped.
+    # Connects to the relay_nodes
+    # Handles events as they get triggered by epoll.
+    # Fires alarms that get scheduled.
+    def run(self):
+        self.connect_to_peers()
+
+        try:
+            _, timeout = self.alarm_queue.time_to_next_alarm()
+            while True:
+                # Grab all events.
+                try:
+                    events = self.epoll.poll(timeout)
+                except IOError as ioe:
+                    if ioe.errno == errno.EINTR:
+                        log_verbose("got interrupted in epoll")
+                        continue
+                    raise ioe
+
+                for fileno, event in events:
+                    conn = self.connection_pool.get_byfileno(fileno)
+
+                    if conn is not None:
+                        # Mark this connection for close if we received a POLLHUP. No other functions will be called
+                        #   on this connection.
+                        if event & select.EPOLLHUP:
+                            conn.state |= ConnectionState.MARK_FOR_CLOSE
+
+                        if event & select.EPOLLOUT and not conn.state & ConnectionState.MARK_FOR_CLOSE:
+                            # If connect received EINPROGRESS, we will receive an EPOLLOUT if connect succeeded
+                            if not conn.state & ConnectionState.INITIALIZED:
+                                conn.state = conn.state | ConnectionState.INITIALIZED
+
+                            # Mark the connection as sendable and send as much as we can from the outputbuffer.
+                            conn.mark_sendable()
+                            conn.send()
+
+                    # handle incoming connection on the server port
+                    elif fileno == self.serversocketfd:
+                        self.handle_serversocket_connections()
+
+                    else:
+                        assert False, "Connection not handled!"
+
+                # Handle EPOLLIN events.
+                for fileno, event in events:
+                    # we already handled the new connections above, no need to handle them again
+                    if fileno != self.serversocketfd:
+                        conn = self.connection_pool.get_byfileno(fileno)
+
+                        if event & select.EPOLLIN and not conn.state & ConnectionState.MARK_FOR_CLOSE:
+                            # log_debug("Node.run", "recv event on {0}".format(conn.peer_desc))
+                            conn.recv()
+
+                        # Done processing. Close socket if it got put on the blacklist or was marked for close.
+                        if conn.state & ConnectionState.MARK_FOR_CLOSE:
+                            log_debug("Connection to {0} closing".format(conn.peer_desc))
+                            self.destroy_conn(fileno)
+                            if conn.is_persistent:
+                                self.alarm_queue.register_alarm(RETRY_INTERVAL, self.retry_init_client_socket, None,
+                                                                conn.__class__, conn.peer_ip, conn.peer_port, True)
+
+                timeout = self.alarm_queue.fire_ready_alarms(not events)
+
+        # Handle shutdown of this node.
+        finally:
+            self.cleanup_node()
 
 
 # A group of connections with active sockets
