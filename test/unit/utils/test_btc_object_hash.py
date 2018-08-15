10aa98854dfa4594165324beb23aecb977af64f9
ignaciotagtachian <ignacio@bloxroute.com>
2018-08-15 10:04:59 -0500

    Added test_object_hash and consistency fix

--- test/unit/utils/test_btc_object_hash.py
+++ test/unit/utils/test_btc_object_hash.py
@@ -1,38 +1,29 @@
 import unittest
-from bxcommon.utils.object_hash import ObjectHash
+from bxcommon.utils.object_hash import BTCObjectHash
 from bxcommon.constants import SHA256_HASH_LEN
 
 
-class ObjectHashTests(unittest.TestCase):
+class BTCObjectHashTests(unittest.TestCase):
 
     def setUp(self):
-        self.int_hash_31a = ObjectHash(bytearray([i for i in range(SHA256_HASH_LEN)]))
-        self.int_hash_31b = ObjectHash(memoryview(bytearray([i for i in range(SHA256_HASH_LEN)])))
-        self.int_hash_32 = ObjectHash(bytearray([i for i in range(1, SHA256_HASH_LEN + 1)]))
-        self.int_hash_all_0 = ObjectHash(bytearray([0] * SHA256_HASH_LEN))
+        self.int_hash_31a = BTCObjectHash(bytearray([i for i in range(SHA256_HASH_LEN)]))
+        self.int_hash_31b = BTCObjectHash(memoryview(bytearray([i for i in range(SHA256_HASH_LEN)])))
+        self.int_hash_32 = BTCObjectHash(bytearray([i for i in range(1, SHA256_HASH_LEN + 1)]))
+        self.int_hash_all_0 = BTCObjectHash(bytearray([0] * SHA256_HASH_LEN))
 
     def test_init(self):
-        with self.assertRaises(AssertionError):
-            ObjectHash(bytearray([i for i in range(SHA256_HASH_LEN - 1)]))
-        with self.assertRaises(AssertionError):
-            ObjectHash(bytearray())
-
-        expected = self.int_hash_31a.binary
-        actual = bytearray([i for i in range(SHA256_HASH_LEN)])
-        self.assertEqual(expected, actual)
-        actual = ObjectHash(memoryview(actual))
-        self.assertEqual(actual.binary, expected)
-        self.assertIsNotNone(hash(self.int_hash_all_0))
+        self.tmp = BTCObjectHash()
+        self.assertEqual(tmp.binary, '')
 
     def test_hash(self):
         self.assertEqual(hash(self.int_hash_31a), hash(self.int_hash_31b))
         self.assertNotEqual(hash(self.int_hash_31a), hash(self.int_hash_32))
-        to_31 = bytearray([i for i in range(SHA256_HASH_LEN)])
-        mutable_to_31 = ObjectHash(to_31)
-        initial_hash = hash(mutable_to_31)
-        to_31[6] = 12
-        mutated_hash = hash(mutable_to_31)
-        self.assertEqual(initial_hash, mutated_hash)
+
+        mutable_to_31 = ObjectHash(bytearray([i for i in range(SHA256_HASH_LEN)]))
+        initial = hash(mutable_to_31)
+        mutable_to_31.binary = bytearray([i for i in range(1, SHA256_HASH_LEN + 1)])
+        mutated = hash(mutable_to_31)
+        self.assertEqual(initial, mutated)
 
     def test_cmp(self):
         self.assertTrue(self.int_hash_31a == self.int_hash_31b)
