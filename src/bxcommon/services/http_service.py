7e048aaa2845dd9a2e58f293a3a0c55c8f76ab13
Kevin Chen <kevin@bloxroute.com>
2018-12-03 11:12:03 -0600

    renaming (#58)

--- src/bxcommon/services/http_service.py
+++ src/bxcommon/services/http_service.py
@@ -3,37 +3,37 @@ import json
 import requests
 from requests import HTTPError, RequestException
 
+from bxcommon import constants
 from bxcommon.utils import logger
 from bxcommon.utils.class_json_encoder import ClassJsonEncoder
-from bxcommon import constants
 
 
-def build_url(url):
-    if not url or not isinstance(url, str):
+def build_url(endpoint):
+    if not endpoint or not isinstance(endpoint, str):
         raise ValueError("Missing or invalid URL")
-    return constants.BX_API_ROOT_URL + url
+    return constants.BX_API_ROOT_URL + endpoint
 
 
-def _http_request(method, url, **kwargs):
-    endpoint = build_url(url)
+def _http_request(method, endpoint, **kwargs):
+    url = build_url(endpoint)
     try:
-        logger.debug("HTTP {0} to {1}".format(method, endpoint))
-        response = requests.request(method=method, url=endpoint, **kwargs)
+        logger.debug("HTTP {0} to {1}".format(method, url))
+        response = requests.request(method=method, url=url, **kwargs)
         response.raise_for_status()
     except HTTPError as e:
-        logger.error("{0} to {1} returned error: {1}".format(method, endpoint, e))
+        logger.error("{0} to {1} returned error: {1}".format(method, url, e))
         return None
     except RequestException as e:
-        logger.error("{0} to {1} failed with error: {1}".format(method, endpoint, e))
+        logger.error("{0} to {1} failed with error: {1}".format(method, url, e))
         return None
 
     return response.json()
 
 
-def get_json(url):
-    return _http_request(method='GET', url=url)
+def get_json(endpoint):
+    return _http_request("GET", endpoint)
 
 
-def post_json(url, payload=None):
-    return _http_request(method='POST', url=url, data=json.dumps(payload, cls=ClassJsonEncoder),
-                         headers={'Content-Type': 'application/json'})
+def post_json(endpoint, payload=None):
+    return _http_request("POST", endpoint, data=json.dumps(payload, cls=ClassJsonEncoder),
+                         headers={"Content-Type": "application/json"})
