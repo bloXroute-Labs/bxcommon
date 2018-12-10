1e0295e6fefc7796d06b5cabfade456f80801ea2
Kevin Chen <kevin@bloxroute.com>
2018-12-10 10:40:56 -0600

    BX-315 Move BTC message classes from bxcommon to bxgateway (#67)
    These aren't used in bxrelay, so moving it to bxgateway.
    
    Precursor to some blockchain sync work I'm doing –– pulling it out to reduce clutter.

--- src/bxcommon/constants.py
+++ src/bxcommon/constants.py
@@ -80,35 +80,6 @@ NETWORK_NUM_LEN = UL_INT_SIZE_IN_BYTES
 # Expiration time for block broadcast message if services info is missing
 MISSING_BLOCK_EXPIRE_TIME = 60
 
-btc_magic_numbers = {
-    "main": 0xD9B4BEF9,
-    "testnet": 0xDAB5BFFA,
-    "testnet3": 0x0709110B,
-    "regtest": 0xDAB5BFFA,
-    "namecoin": 0xFEB4BEF9
-}
-
-# The length of everything in the header minus the checksum
-BTC_HEADER_MINUS_CHECKSUM = 20
-BTC_HDR_COMMON_OFF = 24
-BTC_BLOCK_HDR_SIZE = 81
-# Length of a sha256 hash
-BTC_SHA_HASH_LEN = 32
-BTC_IP_ADDR_PORT_SIZE = 18
-
-# The services that we provide
-# 1: can ask for full blocks.
-# 0x20: Node that is compatible with the hard fork.
-BTC_CASH_SERVICE_BIT = 0x20  # Bitcoin cash service bit
-BTC_NODE_SERVICES = 1
-BTC_CASH_SERVICES = 33
-
-BTC_OBJTYPE_TX = 1
-BTC_OBJTYPE_BLOCK = 2
-BTC_OBJTYPE_FILTERED_BLOCK = 3
-
-BTC_HELLO_MESSAGES = ["version", "verack"]
-
 CUT_THROUGH_TIMEOUT = 60  # Maximum time (in seconds) that we wait for the remote to send us this block
 MGR_DELETE_DELAY = 100  # Time (in seconds) we wait until we delete this manager from our node.
 
