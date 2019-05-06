b97bce0e3303d89c4e55f84069d92d84209f9ea4
Shane van Coller <vc.shane@gmail.com>
2019-05-06 14:25:46 -0500

    Merge changes from develop into release (#194)
    - Adding option to enable buffered send of bytes from relay (#190)
    - Cut-through network optimizations (#192)
        - Cut-through network optimizations
        - Enable buffering by default; Enable buffering only on internal connections
    - display gateway that have no connection to their blockchain node (#185)
    - Version bump -> v1.13.2.0

--- src/bxcommon/services/sdn_http_service.py
+++ src/bxcommon/services/sdn_http_service.py
@@ -96,8 +96,12 @@ def submit_node_offline_event(node_id):
 
 
 def submit_peer_connection_error_event(node_id, peer_ip, peer_port):
+    submit_peer_connection_event(NodeEventType.PEER_CONN_ERR, node_id, peer_ip, peer_port)
+
+
+def submit_peer_connection_event(event_type, node_id, peer_ip, peer_port):
     submit_node_event(
-        NodeEventModel(node_id=node_id, event_type=NodeEventType.PEER_CONN_ERR, peer_ip=peer_ip, peer_port=peer_port))
+        NodeEventModel(node_id=node_id, event_type=event_type, peer_ip=peer_ip, peer_port=peer_port))
 
 
 def submit_gateway_inbound_connection(node_id, peer_id):
