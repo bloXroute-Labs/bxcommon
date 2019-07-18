83b999c671e4b6d96b7979440ed1bd039b2534c9
Kevin Chen <kevin@bloxroute.com>
2019-07-18 10:40:06 -0500

    Add info from SDN on split relays (#300)

--- src/bxcommon/models/node_model.py
+++ src/bxcommon/models/node_model.py
@@ -8,7 +8,7 @@ class NodeModel:
                  sid_start=None, sid_end=None, sid_expire_time=None, last_pong_time=None, is_gateway_miner=None,
                  is_internal_gateway=None, source_version=None, protocol_version=None, blockchain_network_num=None,
                  blockchain_ip=None, blockchain_port=None, node_public_key=None, hostname=None, sdn_id=None,
-                 os_version=None, continent=None, country=None, idx: int = None):
+                 os_version=None, continent=None, country=None, split_relays=None, idx: int = None):
         self.external_port = external_port
         self.network = network
         self.online = online
@@ -30,6 +30,7 @@ class NodeModel:
         self.sdn_id = sdn_id
         self.os_version = os_version
         self.continent = continent
+        self.split_relays = split_relays
         if country is not None:
             self.country = country[:MAX_COUNTRY_LENGTH]
         else:
