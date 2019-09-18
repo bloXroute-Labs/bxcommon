b788820f08881e56c7fd360aa319ffdec52f5297
avrahamblx <46294481+avrahamblx@users.noreply.github.com>
2019-09-18 15:28:58 +0300

    added size calculation method to task queue proxy (#386)

--- src/bxcommon/utils/proxy/task_queue_proxy.py
+++ src/bxcommon/utils/proxy/task_queue_proxy.py
@@ -1,10 +1,14 @@
 import typing
+from typing import Optional, Set
 from collections import deque
+
 import task_pool_executor as tpe  # pyre-ignore for now, figure this out later (stub file or Python wrapper?)
 
+from bxcommon.utils.memory_utils import SpecialMemoryProperties, SpecialTuple
 
-class TaskQueueProxy:
-    QUEUE_GROW_SIZE = 10
+
+class TaskQueueProxy(SpecialMemoryProperties):
+    QUEUE_GROW_SIZE = 1  # TODO: increase this after moving to async task execution
 
     def __init__(
             self, task_creator: typing.Callable[[], tpe.MainTaskBase],
@@ -27,6 +31,14 @@ class TaskQueueProxy:
 
     def return_task(self, tsk: tpe.MainTaskBase):
         self._queue.appendleft(tsk)
+
+    def special_memory_size(self, ids: Optional[Set[int]] = None) -> SpecialTuple:
+        total_size = 0
+        if ids is None:
+            ids = set()
+        for tsk in self._queue:
+            total_size += tsk.get_task_byte_size()
+        return SpecialTuple(total_size, ids)
 
     def _extend_queue(self) -> tpe.MainTaskBase:  # pyre-ignore
         tsk = None
