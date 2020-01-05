0143065c77b5fbfccaa3bd8e6d19f7e5273e5196
sinai yoktan <44772610+sinai-yoktan@users.noreply.github.com>
2020-01-05 11:27:59 +0200

    added to string method (#599)

--- src/bxcommon/connections/connection_state.py
+++ src/bxcommon/connections/connection_state.py
@@ -7,3 +7,6 @@ class ConnectionState(IntFlag):
     HELLO_RECVD = 2
     HELLO_ACKD = 4
     ESTABLISHED = INITIALIZED | HELLO_RECVD | HELLO_ACKD
+
+    def __str__(self):
+        return self.name
--- src/bxcommon/network/network_direction.py
+++ src/bxcommon/network/network_direction.py
@@ -4,3 +4,6 @@ from enum import Enum
 class NetworkDirection(Enum):
     INBOUND = 1
     OUTBOUND = 2
+
+    def __str__(self):
+        return self.name
