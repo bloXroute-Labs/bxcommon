92e3816b270f7cef5dad1cbe4eac6dcb4fa907fa
Sergey Ilin <ilinserg@gmail.com>
2020-02-04 12:24:47 -0600

    BX-1453 Performance profiling (#660)

--- src/bxcommon/connections/abstract_node.py
+++ src/bxcommon/connections/abstract_node.py
@@ -7,6 +7,8 @@ from collections import defaultdict
 from ssl import SSLContext
 from typing import List, Optional, Tuple, Dict, NamedTuple, Union, Set
 
+from pyinstrument import Profiler
+
 from bxcommon import constants
 from bxcommon.connections.abstract_connection import AbstractConnection
 from bxcommon.connections.connection_pool import ConnectionPool
@@ -16,21 +18,21 @@ from bxcommon.exceptions import TerminationError
 from bxcommon.messages.abstract_message import AbstractMessage
 from bxcommon.models.blockchain_network_model import BlockchainNetworkModel
 from bxcommon.models.node_type import NodeType
+from bxcommon.models.outbound_peer_model import OutboundPeerModel
 from bxcommon.network.ip_endpoint import IpEndpoint
 from bxcommon.network.peer_info import ConnectionPeerInfo
 from bxcommon.network.socket_connection_protocol import SocketConnectionProtocol
 from bxcommon.network.socket_connection_state import SocketConnectionState
-from bxcommon.services.threaded_request_service import ThreadedRequestService
 from bxcommon.services.broadcast_service import BroadcastService, BroadcastOptions
+from bxcommon.services.threaded_request_service import ThreadedRequestService
 from bxcommon.utils import memory_utils, json_utils, convert
 from bxcommon.utils.alarm_queue import AlarmQueue
+from bxcommon.utils.config import get_data_file
 from bxcommon.utils.stats.block_statistics_service import block_stats
 from bxcommon.utils.stats.memory_statistics_service import memory_statistics
 from bxcommon.utils.stats.node_info_service import node_info_statistics
 from bxcommon.utils.stats.throughput_service import throughput_statistics
 from bxcommon.utils.stats.transaction_statistics_service import tx_stats
-from bxcommon.models.outbound_peer_model import OutboundPeerModel
-
 from bxutils import logging
 from bxutils.exceptions.connection_authentication_error import ConnectionAuthenticationError
 from bxutils.logging import LogRecordType
@@ -111,6 +113,9 @@ class AbstractNode:
             constants.TX_SERVICE_CHECK_NETWORKS_SYNCED_S, self._transaction_sync_timeout)
 
         self.requester = ThreadedRequestService(self.alarm_queue, constants.THREADED_HTTP_POOL_SLEEP_INTERVAL_S)
+
+        self.profiler = Profiler()
+        self.profiler_enabled = False
 
     def get_sdn_address(self):
         """
@@ -284,6 +289,8 @@ class AbstractNode:
 
     async def close(self):
         logger.info("Node is closing! Closing everything.")
+        if self.profiler_enabled:
+            self.stop_profiler()
         shutdown_task = asyncio.ensure_future(self.close_all_connections())
         try:
             await asyncio.wait_for(shutdown_task, constants.NODE_SHUTDOWN_TIMEOUT_S)
@@ -426,6 +433,30 @@ class AbstractNode:
     def get_target_ssl_ctx(self, endpoint: IpEndpoint, connection_type: ConnectionType) -> SSLContext:
         logger.trace("Fetching SSL certificate for: {} ({}).", endpoint, connection_type)
         return self.node_ssl_service.create_ssl_context(SSLCertificateType.PRIVATE)
+
+    def start_profiler(self, duration_s: int):
+        if self.profiler_enabled:
+            logger.debug("Profiler is already running. Ignore.")
+            return
+
+        logger.info("Starting profiler to run for {} seconds.", duration_s)
+        self.profiler.start()
+        self.profiler_enabled = True
+        self.alarm_queue.register_alarm(duration_s, self.stop_profiler)
+
+    def stop_profiler(self):
+        if not self.profiler_enabled:
+            logger.debug("Profiler is not running. Ignore.")
+            return
+
+        logger.info("Stopping profiler.")
+        self.profiler.stop()
+        output_html = self.profiler.output_html()
+        profile_output_file = get_data_file(f"profile_{os.getpid()}_{time.time()}.html")
+        logger.info(f"Saving profiling output to the file {profile_output_file}")
+        with open(profile_output_file, "w") as profile_output_file:
+            profile_output_file.write(output_html)
+        self.profiler_enabled = False
 
     @abstractmethod
     def _sync_tx_services(self):
