1eef5a906824e62edb980836dceb8daff33d170e
kstola-bloxroute <55707988+kstola-bloxroute@users.noreply.github.com>
2020-02-20 05:41:22 -0600

    Added statistics to performance utils log message (#707)
    * Added statistics to performance utils log message
    
    * Added threshold, type, and kwargs. Removed debug message
    
    * Changed kwargs key name

--- src/bxcommon/utils/performance_utils.py
+++ src/bxcommon/utils/performance_utils.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from bxutils.logging import CustomLogger
@@ -9,5 +10,14 @@ def log_operation_duration(logger: CustomLogger, operation_name: str, start_time
     logger.trace("Performance: {} took {:.3f} s to execute. {}", operation_name, duration, kwargs)
 
     if duration > duration_warn_threshold:
-        logger.debug("Performance Warning: {} took over defined threshold ({} s) to execute, {:.3f}s. {}",
-                     operation_name, duration_warn_threshold, duration, kwargs)
+
+        logger.statistics(
+            {
+                "type": "MessageHandling",
+                "start_time": datetime.utcnow(),
+                "operation": operation_name,
+                "duration": duration,
+                "duration_warn_threshold": duration_warn_threshold,
+                "data": kwargs
+            }
+        )
