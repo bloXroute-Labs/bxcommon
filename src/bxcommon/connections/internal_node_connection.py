23efe291bb99efc17b7de6aa38fa4ac0f1876b73
Kevin Chen <kevin@bloxroute.com>
2020-08-25 02:49:50 -0500

    add ability to check internal gateway conneciton type (#1046)

--- src/bxcommon/connections/internal_node_connection.py
+++ src/bxcommon/connections/internal_node_connection.py
@@ -213,6 +213,9 @@ class InternalNodeConnection(AbstractConnection[Node]):
         # self.CONNECTION_TYPE == ConnectionType.GATEWAY is equal True only for V1 gateways
         return self.CONNECTION_TYPE in ConnectionType.EXTERNAL_GATEWAY or self.CONNECTION_TYPE == ConnectionType.GATEWAY
 
+    def is_internal_gateway_connection(self) -> bool:
+        return self.CONNECTION_TYPE in ConnectionType.INTERNAL_GATEWAY
+
     def is_relay_connection(self):
         return self.CONNECTION_TYPE in ConnectionType.RELAY_ALL
 
