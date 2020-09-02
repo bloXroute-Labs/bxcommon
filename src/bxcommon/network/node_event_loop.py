43120c6eb5d04afb6aa8c55311abdca45330360c
Danielle <44942738+daniellemachpud@users.noreply.github.com>
2020-09-02 10:54:21 +0300

    set connecting state instead of checking the connection (#1064)

--- src/bxcommon/network/node_event_loop.py
+++ src/bxcommon/network/node_event_loop.py
@@ -10,6 +10,7 @@ from typing import Iterator, List, Coroutine, Generator, Callable, Awaitable, Op
 
 from bxcommon import constants
 from bxcommon.connections.abstract_node import AbstractNode
+from bxcommon.connections.connection_state import ConnectionState
 from bxcommon.connections.connection_type import ConnectionType
 from bxcommon.network.abstract_socket_connection_protocol import AbstractSocketConnectionProtocol
 from bxcommon.network.ip_endpoint import IpEndpoint
@@ -164,10 +165,7 @@ class NodeEventLoop:
                 err = repr(e)
 
             self._node.log_refused_connection(peer_info, err)
-            connection = self._node.connection_pool.get_by_ipport(
-                peer_info.endpoint.ip_address, peer_info.endpoint.port
-            )
-            self._node.handle_connection_closed(True, peer_info, connection.state)
+            self._node.handle_connection_closed(True, peer_info, ConnectionState.CONNECTING)
 
     def _iter_outbound_peers(self) -> Generator[ConnectionPeerInfo, None, None]:
         sdn_address = self._node.get_sdn_address()
