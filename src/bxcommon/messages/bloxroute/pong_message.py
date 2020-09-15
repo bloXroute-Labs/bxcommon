34c49aa7f116dbe18462b4aa775912772f81eae1
sinai yoktan <44772610+sinai-yoktan@users.noreply.github.com>
2020-09-15 09:45:36 +0300

    annotations (#1085)
    * annotations

--- src/bxcommon/messages/bloxroute/pong_message.py
+++ src/bxcommon/messages/bloxroute/pong_message.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from bxcommon import constants
 from bxcommon.messages.bloxroute.bloxroute_message_type import BloxrouteMessageType
 from bxcommon.messages.bloxroute.keep_alive_message import KeepAliveMessage
@@ -18,25 +20,28 @@ class PongMessage(KeepAliveMessage):
     )
     PONG_MESSAGE_LENGTH = PONG_MESSAGE_BLOCK.size + constants.CONTROL_FLAGS_LEN
 
-    def __init__(self, nonce=None, timestamp=None, buf=None) -> None:
+    def __init__(
+        self, nonce: Optional[int] = None, timestamp: Optional[int] = None, buf: Optional[bytearray] = None
+    ) -> None:
         if buf is None:
             buf = bytearray(self.HEADER_LENGTH + self.PONG_MESSAGE_LENGTH)
 
         buf = self.PONG_MESSAGE_BLOCK.build(buf, timestamp=timestamp)
 
-        self._timestamp = None
+        self._timestamp: Optional[int] = None
         super(PongMessage, self).__init__(
             msg_type=self.MESSAGE_TYPE, nonce=nonce, buf=buf, payload_length=self.PONG_MESSAGE_LENGTH
         )
 
-    def __unpack(self):
+    def __unpack(self) -> None:
         contents = self.PONG_MESSAGE_BLOCK.read(self._memoryview)
         self._nonce = contents.get("nonce")
         self._timestamp = contents.get("timestamp")
 
-    def timestamp(self):
+    def timestamp(self) -> Optional[int]:
         if self._timestamp is None:
             self.__unpack()
+        assert self._timestamp is not None
         return self._timestamp
 
     def __repr__(self) -> str:
