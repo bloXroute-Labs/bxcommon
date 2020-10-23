a31d48f022758cbc43d0020cb2e0c187f2c2481d
sergeyi23 <ilinserg@gmail.com>
2020-10-23 12:06:15 -0500

    Move base feed logic to bxcommon

---
+++ src/bxcommon/log_messages.py
@@ -0,0 +1,15 @@
+from bxutils.log_message_categories import PROCESSING_FAILED_CATEGORY, GENERAL_CATEGORY
+from bxutils.logging_messages_utils import LogMessage
+
+COULD_NOT_SERIALIZE_FEED_ENTRY = LogMessage(
+    "C-000079",
+    PROCESSING_FAILED_CATEGORY,
+    "Could not serialize feed entry. Skipping."
+)
+
+BAD_FEED_SUBSCRIBER = LogMessage(
+    "C-000068",
+    GENERAL_CATEGORY,
+    "Subscriber {} was not receiving messages and emptying its queue from "
+    "{}. Disconnecting.",
+)
