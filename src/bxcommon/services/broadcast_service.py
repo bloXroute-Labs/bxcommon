e367b1becf452a1b2c64dfc45216be0abbb8b4bc
Kevin Chen <kevin@bloxroute.com>
2020-11-12 14:24:56 -0600

    BX-2223 Don't delay paid txs (#1190)
    * BX-2223 refactor broadcast service
    
    * BX-2223 more minor refactors
    
    * BX-2223 fix test cases
    
    * BX-2223 type errors
    
    * BX-2223 unused imports

--- src/bxcommon/services/broadcast_service.py
+++ src/bxcommon/services/broadcast_service.py
@@ -1,5 +1,4 @@
 from abc import abstractmethod, ABC
-from dataclasses import dataclass, field
 from typing import Optional, List, Iterable, TypeVar, Generic
 
 from bxcommon.connections.abstract_connection import AbstractConnection
@@ -8,25 +7,40 @@ from bxcommon.connections.connection_type import ConnectionType
 from bxcommon.messages.abstract_message import AbstractMessage
 from bxutils import logging
 
-MT = TypeVar("MT", bound=AbstractMessage)
-CT = TypeVar("CT", bound=AbstractConnection)
 logger = logging.get_logger(__name__)
 
 
-@dataclass
 class BroadcastOptions:
-    broadcasting_connection: Optional[AbstractConnection] = None
-    prepend_to_queue: bool = False
-    connection_types: Iterable[ConnectionType] = field(default_factory=list)
+    broadcasting_connection: Optional[AbstractConnection]
+    prepend_to_queue: bool
+    connection_types: Iterable[ConnectionType]
+
+    def __init__(
+        self,
+        broadcast_connection: Optional[AbstractConnection] = None,
+        prepend_to_queue: bool = False,
+        connection_types: Optional[Iterable[ConnectionType]] = None
+    ) -> None:
+        if connection_types is None:
+            connection_types = []
+
+        self.broadcasting_connection = broadcast_connection
+        self.prepend_to_queue = prepend_to_queue
+        self.connection_types = connection_types
 
 
-class BroadcastService(Generic[MT, CT], ABC):
+MT = TypeVar("MT", bound=AbstractMessage)
+CT = TypeVar("CT", bound=AbstractConnection)
+OT = TypeVar("OT", bound=BroadcastOptions)
+
+
+class BroadcastService(Generic[MT, CT, OT], ABC):
     connection_pool: ConnectionPool
 
     def __init__(self, connection_pool: ConnectionPool) -> None:
         self.connection_pool = connection_pool
 
-    def broadcast(self, message: MT, options: BroadcastOptions) -> List[CT]:
+    def broadcast(self, message: MT, options: OT) -> List[CT]:
         if options.broadcasting_connection is not None:
             logger.log(message.log_level(), "Broadcasting {} to [{}] connections from {}.",
                        message, ",".join(map(str, options.connection_types)), options.broadcasting_connection)
@@ -38,22 +52,25 @@ class BroadcastService(Generic[MT, CT], ABC):
         return self.broadcast_to_connections(message, connections, options)
 
     @abstractmethod
-    def should_broadcast_to_connection(self, message: MT, connection: CT) -> bool:
+    def should_broadcast_to_connection(
+        self, message: MT, connection: CT, options: OT
+    ) -> bool:
         pass
 
-    def get_connections_for_broadcast(self, message: MT, options: BroadcastOptions) -> List[CT]:
+    def get_connections_for_broadcast(self, message: MT, options: OT) -> List[CT]:
         connections = []
-        # pyre-fixme[6]: Expected `List[ConnectionType]` for 1st param but got `Iterable[ConnectionType]`.
-        for connection in self.connection_pool.get_by_connection_types(options.connection_types):
+        for connection in self.connection_pool.get_by_connection_types(
+            options.connection_types
+        ):
             if (
-                self.should_broadcast_to_connection(message, connection) and
+                self.should_broadcast_to_connection(message, connection, options) and
                 connection != options.broadcasting_connection
             ):
                 connections.append(connection)
         return connections
 
     def broadcast_to_connections(
-        self, message: AbstractMessage, connections: Iterable[CT], options: BroadcastOptions
+        self, message: AbstractMessage, connections: Iterable[CT], options: OT
     ) -> List[CT]:
         broadcast_connections = []
         for connection in connections:
